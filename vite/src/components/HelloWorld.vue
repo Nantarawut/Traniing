e34c5ec205ed312ff44924389dfff0cde8cf8de5
PETCHLUVFAMILY <125719853+Nantarawut@users.noreply.github.com>
2023-03-18 14:16:55 +0700

    Chang commit

--- vite/src/components/HelloWorld.vue
+++ vite/src/components/HelloWorld.vue
@@ -3,13 +3,19 @@ import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
+const message = ref('')
 const count = ref(0)
+
+const add =() =>
+  count.value++
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ msg + ' ' + message }}</h1>
 
   <div class="card">
+    <input type="text" v-model="message">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
@@ -21,7 +27,7 @@ const count = ref(0)
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
-    >, the official Vue + Vite starter
+    >, the official Vue + Vite +  starter
   </p>
   <p>
     Install
